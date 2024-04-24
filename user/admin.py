60fe7b093367cd344a7e4a9f473a7a3db039cec1
jenisha9 <njenisha78@gmail.com>
2024-04-24 17:53:09 +0545

    feat: register new models

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CustomUser
+from .models import CartLine, CustomUser, Cart
 admin.site.register(CustomUser)
+admin.site.register(Cart)
+admin.site.register(CartLine)
