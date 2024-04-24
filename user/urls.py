e9f5f07d5b747211bf0fa06580a43714c40fcdd6
jenisha9 <njenisha78@gmail.com>
2024-04-24 17:51:45 +0545

    fix: fix routes

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import RegisterView,LoginView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LoginView, RegisterView, CartView, LogoutView
+
+router = DefaultRouter()
+router.register(r'cart', CartView, basename='cart')
 
 urlpatterns = [
-    path('register/',RegisterView.as_view(), name='register'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+     path('logout/', LogoutView.as_view(), name='logout'),
+    path('', include(router.urls)),
 ]
+
+
+
