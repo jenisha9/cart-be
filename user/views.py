98542564c597542ac499f08afe6af1e15a20f618
jenisha9 <njenisha78@gmail.com>
2024-04-25 12:55:12 +0545

    feat: add function with endpoint for get from cart

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from product.models import Product
+from product.serializer import ProductSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.authentication import TokenAuthentication
@@ -8,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import logout
 from rest_framework.decorators import action
+from rest_framework import status
 
 from .models import Cart, CartLine, CustomUser
 from .serializer import CartSerializer, UserSerializer
@@ -41,27 +43,56 @@ class LogoutView(APIView):
         logout(request)
         return Response({'message': 'Logout successful'})
 
-
 class CartView(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
 
+    @action(detail=False, methods=["GET"], url_path="get-from-cart")
+    def get_from_cart(self, request):
+        user = request.user
+        cart = Cart.objects.get_or_create(user=user)[0]
+        cart_lines = cart.cart_lines.all()
+
+        serialized_cart = []
+        for cart_line in cart_lines:
+            serialized_cart_line = {
+                'product': ProductSerializer(cart_line.product).data,  
+                'quantity': cart_line.quantity
+            }
+            serialized_cart.append(serialized_cart_line)
+
+        return Response(serialized_cart)
+    
     @action(detail=False, methods=["POST"], url_path="add-to-cart")
     def add_to_cart(self, request):
         user = request.user
-        serializer = CartSerializer(data=request.data)
-        if serializer.is_valid():
-            product_id = serializer.validated_data.get("product_id")
-            quantity = serializer.validated_data.get("quantity", 1)
-            try:
-                product = Product.objects.get(pk=product_id)
-                cart = Cart.objects.get_or_create(user=user)[0]
-                cart_line, created = CartLine.objects.get_or_create(product=product, cart=cart)
-                if not created:
-                    cart_line.quantity += quantity
-                    cart_line.save()
-                return Response({"message": "Product added to cart"})
-            except Product.DoesNotExist:
-                return Response({"error": "Product not found"}, status=404)
+        validate = CartSerializer(data=request.data)
+        product_id = request.data.get("product_id")
+        quantity = request.data.get("quantity",1)
+        product = Product.objects.get(pk=product_id)
+        cart = Cart.objects.get_or_create(user=user)[0]
+        
+        cart_line_qs = user.cart.cart_lines.filter(product=product)
+        if cart_line_qs.exists():
+            cart_line = cart_line_qs.first()
+            cart_line.quantity += quantity
+            cart_line.save()
         else:
-            return Response(serializer.errors, status=400)
+
+            cart_line = CartLine.objects.create(
+                product=product, cart=cart, quantity=quantity
+            )
+        serializer=CartSerializer(cart_line)
+        return Response(serializer.data)
+
+    # @action(detail=False, methods=["DELETE"], url_path="remove-from-cart")
+    # def remove_from_cart(request):
+    #     user = request.user
+    #     product_id = request.data.get("product_id")
+
+    #     try:
+    #         cart_line = user.cart.cart_lines.get(product_id=product_id)
+    #         cart_line.delete()
+    #         return Response({"message": "Item removed from cart."}, status=status.HTTP_204_NO_CONTENT)
+    #     except CartLine.DoesNotExist:
+    #         return Response({"error": "Item not found in cart."}, status=status.HTTP_404_NOT_FOUND)
